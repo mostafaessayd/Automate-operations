d7bf07d1febb4d0a79e488ab51da5e2fa09f1803
mohamedessayd <ballm3957@gmail.com>
2025-02-18 13:59:00 +0100

    new

--- Convert NFA toDFA/controller.js
+++ Convert NFA toDFA/controller.js
@@ -3,4 +3,23 @@ var initialStates = [];
 function addNewInitialStates() {
     var state = parseInt(document.getElementById('Initial-states').value);
     
+    initialStates.push(state);
+    var newInner = ``;
+    for(let i = 0 ; i < initialStates.length ; i++) {
+         newInner += `<div class = "container-of-number">${initialStates[i]}</div>`;
+    }
+    document.getElementById('display-initial-states').innerHTML = newInner;
+}
+
+var finalStates = [];
+// add new initial states
+function addNewFinalStates() {
+    var state = parseInt(document.getElementById('final-states').value);
+    
+    finalStates.push(state);
+    var newInner = ``;
+    for(let i = 0 ; i < finalStates.length ; i++) {
+         newInner += `<div class = "container-of-number">${finalStates[i]}</div>`;
+    }
+    document.getElementById('display-final-states').innerHTML = newInner;
 }
