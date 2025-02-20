7c0ad814eeb74ee1a1794c238fa233668d7e9635
mohamedessayd <ballm3957@gmail.com>
2025-02-20 17:11:40 +0100

    update

--- validate word in automate/inputFileController.js
+++ validate word in automate/inputFileController.js
@@ -132,3 +132,56 @@ function getComponantOfTransition(line) {
 
     return { leftState: leftState, symbol: symbol, rightState: rightState };
 }
+
+var MAX_NUMBER_OF_STATES = 8;
+var MAX_NUMBER_OF_SYMBOLS = 6;
+function generateRandomAutomate() {
+    var numberOfState = 1 + Math.floor(Math.random() * MAX_NUMBER_OF_STATES);
+    var initialState = Math.floor(Math.random() * numberOfState);
+    var finalStates = [];
+    for(let i = 0 ; i < numberOfState ; i++) {
+        var randomState = Math.floor(Math.random() * numberOfState);
+        if(!checkContain(finalStates , randomState)) {
+            finalStates.push(randomState);
+        }
+    }
+    
+    const allCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    var validSymbols = [];
+    var randomSize = 1 + Math.floor(Math.random() * MAX_NUMBER_OF_SYMBOLS);
+    for(let i = 0 ; i < randomSize ; i++) {
+        var randomSymbol = allCharacters[Math.floor(Math.random() * allCharacters.length)];
+        if(!checkContain(validSymbols , randomSymbol)) {
+            validSymbols.push(randomSymbol);
+        }
+    }
+    
+    var leftStateList = [];
+    var symbolList = [];
+    var rightStateList = [];
+    randomSize = 1 + Math.floor(Math.random() * (validSymbols.length * numberOfState));
+    for(let i = 0 ; i < randomSize ; i++) {
+        var randomLeftstate = Math.floor(Math.random() * numberOfState);
+        var randomRightState = Math.floor(Math.random() * numberOfState);
+        var randomSymbol = validSymbols[Math.floor(Math.random() * validSymbols.length)];
+        var found = false;
+        for(let j = 0 ; j < leftStateList.length ; j++) {
+            found |= (leftStateList[j] == randomLeftstate && symbolList[j] == randomSymbol);
+        }
+        if(!found) {
+            leftStateList.push(randomLeftstate);
+            symbolList.push(randomSymbol);
+            rightStateList.push(randomRightState);
+        }
+    }
+
+    document.location = "answer.html";
+    window.location.href = 'answer.html?' 
+    + 'numberOfState=' + encodeURIComponent(numberOfState) 
+    + '&initialState=' + encodeURIComponent(initialState)
+    + '&finalStates=' + encodeURIComponent(finalStates)
+    + '&validSymbols=' + encodeURIComponent(validSymbols)
+    + '&leftStateList=' + encodeURIComponent(leftStateList)
+    + '&symbolList=' + encodeURIComponent(symbolList)
+    + '&rightStateList=' + encodeURIComponent(rightStateList);
+}
